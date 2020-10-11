48e1825a4b414b42a4bb6ebc0923d146fb2b1636
tucelek@gmail.com <tucelek@gmail.com>
2020-10-12 00:31:29 +0200

    almost working

--- script.js
+++ script.js
@@ -11,11 +11,22 @@ const randomNumber = () =>{
   return randomTable[Math.random() * randomTable.length | 0];
 }
 
+const delayOnFading = (callback) =>{
+  setTimeout(() =>{
+    callback();
+  } , 1250)
+}
+
 const ai = () =>{
   const random = randomNumber();
   blocks[random].style.pointerEvents = "none";
   randomTable.splice(randomTable.indexOf(random), 1);
   circles[random].classList.add('show');
+}
+
+const displayResult = (content, color) =>{
+  showResult.textContent = content;
+  showResult.style.color = color;
 }
 
 const clearBoard = () =>{
@@ -35,69 +46,113 @@ crosses.forEach(cross =>{
   cross.className = "fas fa-times cross";
 })
 
-setTimeout(() =>{
-  result();
-},1500)
+delayOnFading(displayResult);
 
 counter = 0;
+i = 0;
 }
 
-const result = (content, color) =>{
-  showResult.textContent = content;
-  showResult.style.color = color;
+
+
+const disableAllBlocks = () =>{
+  blocks.forEach(block =>{
+    block.style.pointerEvents = "none";
+  })
 }
 
 // const highlightOutcome = (b1, b2, b2, color) =>{
+
+
 
 // }
 
 const userWin = () =>{
   if(blocks[0].className[6] && blocks[1].className[6] && blocks[2].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[3].className[6] && blocks[4].className[6] && blocks[5].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[6].className[6] && blocks[7].className[6] && blocks[8].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+     delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[0].className[6] && blocks[3].className[6] && blocks[6].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[1].className[6] && blocks[4].className[6] && blocks[7].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[2].className[6] && blocks[5].className[6] && blocks[8].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[0].className[6] && blocks[4].className[6] && blocks[8].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (blocks[6].className[6] && blocks[4].className[6] && blocks[2].className[6]){
-    result('WIN', 'lightgreen');
-    clearBoard();
-    i++;
+    displayResult('WIN', 'lightgreen');
+      delayOnFading(clearBoard);
+    disableAllBlocks();
   }
   else if (counter == 5){
-    result('DRAW', '#333');
-    setTimeout(() =>{
-      clearBoard();
-    },1500)
-    i++;
+    displayResult('DRAW', '#666');
+    delayOnFading(clearBoard);
+  }
+}
+
+const aiWin = () =>{
+  if(blocks[0].className[6] && blocks[1].className[6] && blocks[2].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[3].className[6] && blocks[4].className[6] && blocks[5].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[6].className[6] && blocks[7].className[6] && blocks[8].className[6]){
+    displayResult('LOSS', 'red');
+     delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[0].className[6] && blocks[3].className[6] && blocks[6].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[1].className[6] && blocks[4].className[6] && blocks[7].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[2].className[6] && blocks[5].className[6] && blocks[8].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[0].className[6] && blocks[4].className[6] && blocks[8].className[6]){
+    displayResult('LOSS', 'red');
+    delayOnFading(clearBoard);
+    disableAllBlocks();
+  }
+  else if (blocks[6].className[6] && blocks[4].className[6] && blocks[2].className[6]){
+    displayResult('LOSS', 'red');
+      delayOnFading(clearBoard);
+    disableAllBlocks();
   }
 }
 
@@ -108,18 +163,32 @@ const userWin = () =>{
       crosses[index].classList.add('show');
       block.className += " 1";
       counter++;
+
       userWin();
-      if(i == 0){
+     
+
+
+      if(i == 0 && index != 4){
+        blocks[4].style.pointerEvents = "none";
+        randomTable.splice(randomTable.indexOf(4), 1);
+        circles[4].classList.add('show');
+      } else{
         ai();
       }
-      i=0;
+        
+      
+
+      i++;
+
     })
   })
+
 
 
   
   clear.addEventListener('click', () =>{
     clearBoard();
+    i=0;
   })
 
   
